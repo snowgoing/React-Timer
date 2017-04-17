3bab4c55fab9775d6afc943913d287807c97f945
snowgoing <snowgoing@gmail.com>
2017-04-17 14:45:10 -0700

    Finalize tests and add page-title

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -78,6 +78,7 @@ class Countdown extends Component {
 
     return (
       <div>
+        <h1 className='page-title'>Countdown App</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -34,5 +34,30 @@ describe('Countdown', () => {
         done();
       }, 3001);
     });
+
+    it('should pause countdown on pause status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout((done) => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should reset count on stopped', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout((done) => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
+
   });
 });
